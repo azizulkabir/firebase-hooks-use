bf21b55b9b088b08cc5fd6802bad25416d49f8c5
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-06 05:07:23 +0600

    btn

--- src/component/pages/Header.js
+++ src/component/pages/Header.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='p-1 mx-auto w-50'>
-            <Link className='mx-1 text-success' to='/'>Home</Link>
-            <Link className='mx-1 text-success' to='/About'>About</Link>
-            <Link className='mx-1 text-success' to='/checkout'>Checkout</Link>
-            <Link className='mx-1 text-success' to='/login'>Login</Link>
-            <Link className='mx-1 text-success' to='/register'>Register</Link>
+            <Link className='btn btn-secondary mx-1' to='/'>Home</Link>
+            <Link className='btn btn-secondary mx-1' to='/About'>About</Link>
+            <Link className='btn btn-secondary mx-1' to='/checkout'>Checkout</Link>
+            <Link className='btn btn-secondary mx-1' to='/login'>Login</Link>
+            <Link className='btn btn-secondary mx-1' to='/register'>Register</Link>
         </div>
     );
 };
