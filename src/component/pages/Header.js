481ea83fd22b9d3ed7d8a1a9ed0cafdf645936c8
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-06 04:56:39 +0600

    header complete

--- src/component/pages/Header.js
+++ src/component/pages/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            this is header 
+        <div className='p-1 mx-auto w-50'>
+            <Link className='mx-1 text-success' to='/'>Home</Link>
+            <Link className='mx-1 text-success' to='/About'>About</Link>
+            <Link className='mx-1 text-success' to='/checkout'>Checkout</Link>
+            <Link className='mx-1 text-success' to='/login'>Login</Link>
+            <Link className='mx-1 text-success' to='/register'>Register</Link>
         </div>
     );
 };
--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Register from '../Athentication/Register';
+import Register from '../Authentication/Register';
 import Login from '../Authentication/Login';
-import About from './about';
+import About from '../Authentication/Login';
+
 import CheckOut from './CheackOut';
+import Header from './Header';
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
            <Routes>
-            <Route path='/' element={<Home></Home>}></Route>
             <Route path='/login' element={<Login></Login>}></Route>
             <Route path='/register' element={<Register></Register>}></Route>
             <Route path='/about' element={<About></About>}></Route>
