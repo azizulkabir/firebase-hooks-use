38807616e611917cba04b12e7d5d26127cdedcd8
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-06 04:27:00 +0600

    react router and route

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Register from '../Athentication/Register';
+import Login from '../Authentication/Login';
+import About from './about';
+import CheckOut from './CheackOut';
 
 const Home = () => {
     return (
         <div>
-            this is home pages
+           <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
+            <Route path='/about' element={<About></About>}></Route>
+            <Route path='/checkout' element={<CheckOut></CheckOut>}></Route>
+           </Routes>
         </div>
     );
 };
