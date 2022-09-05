eec4d9880beeefa295ed83816ba3eca95a942a1e
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-06 04:08:42 +0600

    initialize project

---
+++ src/component/Athenication/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            thius is register pages 
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/component/pages/CheackOut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckOut = () => {
+    return (
+        <div>
+            this is CheckOut checkout
+        </div>
+    );
+};
+
+export default CheckOut;
