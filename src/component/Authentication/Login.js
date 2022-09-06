002f75b8d066dd7ce43284b9235b31f9ebc2631b
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-09-07 01:40:55 +0600

    github authenication

--- src/component/Authentication/Login.js
+++ src/component/Authentication/Login.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useSignInWithGithub, useSignInWithGoogle, } from 'react-firebase-hooks/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const Login = () => {
+    const [signInWithGoogle,  loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGithub,] = useSignInWithGithub(auth);
+    const [user] = useAuthState(auth);
+    
+    if (loading) {
+        return (
+            <div>
+                <p>Loading.....</p>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
+            </div>
+        )
+    }
+
+    if (user) {
+        return (
+            <div>
+                Name: {user.displayName}
+            </div>
+        )
+    }
+
     return (
         <div>
-            this is login page 
+
+            <button onClick={() => signInWithGoogle()}> Google SignIn</button>
+            <button onClick={() => signInWithGithub()}> Github SignIn</button>
+
+
         </div>
     );
 };
--- src/component/pages/Header.js
+++ src/component/pages/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from "firebase/auth";
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+        console.log('clicked sign out')
+    }
     return (
         <div className='p-1 mx-auto w-50'>
             <Link className='btn btn-secondary mx-1' to='/'>Home</Link>
-            <Link className='btn btn-secondary mx-1' to='/About'>About</Link>
+            <Link className='btn btn-secondary mx-1' to='/about'>About</Link>
             <Link className='btn btn-secondary mx-1' to='/checkout'>Checkout</Link>
-            <Link className='btn btn-secondary mx-1' to='/login'>Login</Link>
-            <Link className='btn btn-secondary mx-1' to='/register'>Register</Link>
+
+            {
+                user ? <button className='btn btn-secondary mx-1' onClick={handleSignOut}> SignOut</button>
+                    :
+                    <Link className='btn btn-secondary mx-1' to='/login'> Login</Link>
+
+            }
+
         </div>
     );
 };
